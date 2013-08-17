dc4a22c88489d8bf7f873a8df936b4ccf100ff18
vichargrave <vichargrave@gmail.com>
2013-08-16 17:40:28 -0700

    Reversed order of pthread_cancel() and pthread_detach() calls.

--- thread.cpp
+++ thread.cpp
@@ -31,11 +31,11 @@ Thread::Thread() : m_tid(0), m_running(0), m_detached(0) {}
 
 Thread::~Thread()
 {
-    if (m_running == 1) {
-        pthread_cancel(m_tid);
-    }
     if (m_running == 1 && m_detached == 0) {
         pthread_detach(m_tid);
+    }
+    if (m_running == 1) {
+        pthread_cancel(m_tid);
     }
 }
 
